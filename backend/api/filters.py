c3a4311f9c818e902c6d984d45ea75a5ea2929eb
BKSLab <bks2408@mail.ru>
2023-09-24 15:29:44 +0400

    foodgramProject. Deploy. Version 15. FilterSet

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,7 @@
 from django_filters import rest_framework
+from django_filters.rest_framework import FilterSet, filters
 
-from recipes.models import Ingredient, Recipe
+from recipes.models import Ingredient, Recipe, Tag
 
 
 class IngredientsFilter(rest_framework.FilterSet):
@@ -15,19 +16,23 @@ class IngredientsFilter(rest_framework.FilterSet):
         fields = ('name',)
 
 
-class RecipeFilter(rest_framework.FilterSet):
+class RecipeFilter(FilterSet):
     """Фильтрация рецептов."""
 
-    author = rest_framework.CharFilter(field_name='author__id')
+    author = filters.CharFilter(field_name='author__id')
     # tags = rest_framework.AllValuesMultipleFilter(
     #     queryset=Tag.objects.all(),
     #     field_name='tags__slug',
     #     to_field_name='slug',
     # )
     # AllValuesMultipleFilter(field_name='tags__slug')
-    tags = rest_framework.AllValuesMultipleFilter(field_name='tags__slug')
-    is_favorited = rest_framework.BooleanFilter(method='check_is_favorited')
-    is_in_shopping_cart = rest_framework.BooleanFilter(
+    tags = filters.ModelMultipleChoiceFilter(
+        queryset=Tag.objects.all(),
+        field_name='tags__slug',
+        to_field_name='slug',
+    )
+    is_favorited = filters.BooleanFilter(method='check_is_favorited')
+    is_in_shopping_cart = filters.BooleanFilter(
         method='check_is_in_shopping_cart'
     )
 
