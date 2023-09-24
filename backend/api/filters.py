8e96c050506c280c405935a79ab5fb73fe62da59
BKSLab <bks2408@mail.ru>
2023-09-24 14:39:19 +0400

    foodgramProject. Deploy. Version 15. Filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,6 @@
 from django_filters import rest_framework
 
-from recipes.models import Ingredient, Recipe, Tag
+from recipes.models import Ingredient, Recipe
 
 
 class IngredientsFilter(rest_framework.FilterSet):
@@ -19,13 +19,13 @@ class RecipeFilter(rest_framework.FilterSet):
     """Фильтрация рецептов."""
 
     author = rest_framework.CharFilter(field_name='author__id')
-    tags = rest_framework.AllValuesMultipleFilter(
-        queryset=Tag.objects.all(),
-        field_name='tags__slug',
-        to_field_name='slug',
-    )
+    # tags = rest_framework.AllValuesMultipleFilter(
+    #     queryset=Tag.objects.all(),
+    #     field_name='tags__slug',
+    #     to_field_name='slug',
+    # )
     # AllValuesMultipleFilter(field_name='tags__slug')
-    # tags = rest_framework.CharFilter(field_name='tags__slug')
+    tags = rest_framework.AllValuesMultipleFilter(field_name='tags__slug')
     is_favorited = rest_framework.BooleanFilter(method='check_is_favorited')
     is_in_shopping_cart = rest_framework.BooleanFilter(
         method='check_is_in_shopping_cart'
