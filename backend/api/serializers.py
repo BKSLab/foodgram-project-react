45962f86bb272999514b911dc672a617eecbbeab
BKSLab <bks2408@mail.ru>
2023-09-24 13:16:04 +0400

    foodgramProject. Deploy. Version 13. ingredients

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -202,11 +202,9 @@ class CreateUpdateDeleteRecipeSerializer(serializers.ModelSerializer):
             )
         if check_repetitions([ingredient.get('id') for ingredient in value]):
             raise serializers.ValidationError(
-                {
-                    'ingredients': [
-                        'Повторение ингредиентов не допускается.'
-                    ]
-                }
+                [
+                    'Повторение ингредиентов не допускается.'
+                ]
             )
         return value
 
